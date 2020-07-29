9efe256310dac66806b693ba1e339d3e712429f2
Lino Sa <39319780+rustimacc@users.noreply.github.com>
2020-07-29 18:03:12 -0300

    Se agregó interfaz básica del menu principal y barra de carga del nivel

--- Assets/Scripts/MainController.cs
+++ Assets/Scripts/MainController.cs
@@ -2,11 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.UI;
 public class MainController : MonoBehaviour
 {
+    public GameObject botonEmpezarjuego;
+    public GameObject cargarnivel;
+    public Slider slider;
+
+    AsyncOperation async;
     public void ElegirNivel(string nivel)
     {
-        SceneManager.LoadScene(nivel);
+        StartCoroutine(cargarEscena(nivel));
+        //SceneManager.LoadScene(nivel);
+    }
+    IEnumerator cargarEscena(string nivel)
+    {
+        botonEmpezarjuego.SetActive(false);
+        cargarnivel.SetActive(true);
+        yield return new WaitForSeconds(.5f);
+        async = SceneManager.LoadSceneAsync(nivel);
+        async.allowSceneActivation = false;
+        while (!async.isDone)
+        {
+            slider.value = async.progress;
+            if (async.progress >= .9f)
+            {
+                slider.value = 1;
+                async.allowSceneActivation = true;
+            }
+            yield return null;
+        }
     }
 }
